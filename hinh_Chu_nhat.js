421cedfce000d56db8d1be5302d1e2c78e567f68
Nguyễn Đắc Lâm <62978868+ndlam1996@users.noreply.github.com>
2021-06-03 20:05:43 +0700

    Update hinh_Chu_nhat.js

--- hinh_Chu_nhat.js
+++ hinh_Chu_nhat.js
@@ -1,18 +1,23 @@
 class rec{
   cd = 3;
   cr = 2;
+  constructor(d,r){
+    this.cd = d;
+    this.cr = r;
+  }
 
   acreage(){
-  let acr = this.cr * this.cd;
-    document.write( "dien tich hinh chu nhat " + acr +"</br>");
+    let acr = this.cr * this.cd;
+    return acr;
   }
   perimeter(){
-    let per = (this.cr + this.cd)*2
-    document.write("chu vi hinh chu nhat " + per )
+    let per = (this.cr + this.cd)*2;
+    return per;
+    
   }
 }
-let A = new rec();
-A.cd = 10;
-A.cr = 5 ;
-A.acreage();
-A.perimeter()
+let A = new rec(10,5);
+let acr = A.acreage();
+document.write( "dien tich hinh chu nhat " + acr +"</br>");
+let per = A.perimeter();
+document.write("chu vi hinh chu nhat " + per );
